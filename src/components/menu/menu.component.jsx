815dcc7c839aacf90858b73a3d5255f9fe84e090
Bhargav <bhargav999reddy@gmail.com>
2020-06-07 21:25:08 +0530

    added links to pages

--- src/components/menu/menu.component.jsx
+++ src/components/menu/menu.component.jsx
@@ -2,15 +2,29 @@ import React from "react";
 
 import "./menu.styles.scss";
 
+import { Link } from "react-router-dom";
+
 const Menu = () => (
   <div className="menu">
-    <span className="sm regular">home</span>
+    <Link className="link sm regular card2" to="/">
+      home
+    </Link>
     {/* <span className="sm regular">UI/UX</span> */}
-    <span className="sm regular">web & mobile</span>
-    <span className="sm regular">business intelligence</span>
-    <span className="sm regular">AI and Cloud solutions</span>
-    <span className="sm regular">Contact us</span>
-    <span className="sm regular">About</span>
+    <Link className="link sm regular" to="/web-and-mobile">
+      web & mobile
+    </Link>
+    <Link className="link sm regular" to="/business-intelligence">
+      business intelligence
+    </Link>
+    <Link className="link sm regular" to="/ai-and-cloud-solutions">
+      AI and Cloud solutions
+    </Link>
+    <Link className="link sm regular" to="/contact-us">
+      Contact us
+    </Link>
+    <Link className="link sm regular" to="/about">
+      About
+    </Link>
   </div>
 );
 
